9261833c0a570708df5cead978612e897b5ae383
Vivek-chandravanshi <chandravanshivivek11@gmail.com>
2025-03-22 00:53:33 +0530

    basic conversion and operators

---
+++ 01_basics/03_datatypeConversion.js
@@ -0,0 +1,46 @@
+let num = "44"
+
+// console.log(typeof num)
+// console.log(typeof(num))
+
+let valueInNumber = Number(num)
+// console.log(typeof(valueInNumber))
+
+let num2 = "34abve"
+let valueInNumber2 = Number(num2)
+// console.log(typeof(valueInNumber2))
+// console.log(valueInNumber2)
+
+//null converts to 0 in Number(null)
+
+//true => 1, false=>0
+//"vivek" => Number() = NaN
+//typeof(NaN) = Number
+
+// "" : Boolean("") = false
+// "vivek" : Boolean("vivek") = true
+// Boolean(1) = true
+
+//String(33) = "33" in string
+
+//================================Operations====================================
+let value = 4
+let negValue = -value
+// console.log(negValue)
+// console.log(2**3)
+
+let str1 = "hello"
+let str2 = " vivek"
+let str3 = str1 + str2
+// console.log(str3)
+
+console.log("1"+2)
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+console.log(true)
+console.log(+true)
+console.log("")
+console.log(+"")
+
+//++x and x++ prefix and postfix operator
